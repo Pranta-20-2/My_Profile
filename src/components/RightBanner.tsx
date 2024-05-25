a44c2d48096cdb7bfef067cce27a86ef51e9e06a
Pranta_Das <110590290+Pranta-20-2@users.noreply.github.com>
2024-05-25 17:09:29 +0600

    Convert from private to public

---
+++ src/components/RightBanner.tsx
@@ -0,0 +1,17 @@
+import { bannerImg } from "../assets";
+import { FadeIn } from "./FadeIn";
+
+const RightBanner = () => {
+  return (
+    <FadeIn className="w-full lgl:w-1/2 flex justify-center items-center relative">
+      <img
+        className="w-[250px] h-[300px] lgl:w-[400px] lgl:h-[500px] z-10 rounded-full"
+        src={bannerImg}
+        alt="bannerImg"
+      />
+      <div className="absolute w-[350px] h-[300px] lgl::w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#0B1120] shadow-shadowOne flex justify-center items-center rounded-full"></div>
+    </FadeIn>
+  );
+};
+
+export default RightBanner;
